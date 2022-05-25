1aca8c92ac96eb8e4e19632fe5426bf6dbd6b641
Jon Warrington <jonw@ggbetawerks.com>
2022-05-25 13:35:11 -0600

    Updates to Angular / Ionic, and updated to use the new Ionic Accordian control instead of the home made one.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,25 +9,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { ColorUtilitiesService } from './services/color-utilities.service';
 import { ColorListService } from './services/color-list.service';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { AccordionItemComponent } from './components/accordion-item';
-import { AccordionGroupComponent } from './components/accordion-group';
+
 import { AppTemplateDirective } from './components/app-template';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        SidebarComponent,
-        AccordionItemComponent,
-        AccordionGroupComponent,
-        AppTemplateDirective,
-    ],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-    exports: [AppTemplateDirective],
-    providers: [
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-        ColorUtilitiesService,
-        ColorListService,
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, SidebarComponent, AppTemplateDirective],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  exports: [AppTemplateDirective],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ColorUtilitiesService,
+    ColorListService,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
